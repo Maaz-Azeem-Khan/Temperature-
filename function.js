6edd9d75fbba2fc478cb40659fca5fba27c8ad13
unknown <maazkkhan758@gmail.com>
2024-12-21 13:44:22 +0500

    initial commit

---
+++ function.js
@@ -0,0 +1,17 @@
+$("#btn1").click(function convertTemperature() {
+  let celsiusValue = document.getElementById("input1").value;
+
+  let fahrenheitValue = document.getElementById("input2").value;
+
+  if (celsiusValue !== "") {                                      
+    let fahrenheitValue = (celsiusValue * 9) / 5 + 32;
+
+    document.getElementById("input2").value = fahrenheitValue;
+  } 
+  
+  else if (fahrenheitValue !== "") {
+    let celsiusValue = ((fahrenheitValue - 32) * 5) / 9;
+
+    document.getElementById("input1").value = celsiusValue;
+  }
+});
